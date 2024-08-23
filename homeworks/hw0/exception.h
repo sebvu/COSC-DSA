c437a3585fbb8b8e9040174e04bc34d0a6da6fc4
sebvu <santosjester738@gmail.com>
2024-08-22 21:48:34 -0500

    Remove commented out function in exception.h

--- homeworks/hw0/exception.h
+++ homeworks/hw0/exception.h
@@ -12,11 +12,6 @@ public:
   caseDoesNotExist(std::string area, char type) : colOrRow(area), type(type) {}
   std::string getColOrRow() { return colOrRow; }
   int getType() { return type; }
-
-  // void what() {
-  //   std::cout << "Case does not exist at " << area << " type " << type
-  //             << std::endl;
-  // }
 };
 
 #endif
