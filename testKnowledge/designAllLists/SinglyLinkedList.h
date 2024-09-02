78714a3e725faecbfe801f3646aca144df40baca
sebvu <santosjester738@gmail.com>
2024-09-02 04:30:09 -0500

    Add basic singlyLinkedList implementation

---
+++ testKnowledge/designAllLists/SinglyLinkedList.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <iostream>
+
+template <typename T> struct Node {
+  T data;
+  Node *next;
+  Node(T data, Node *next = nullptr) : data(data), next(next) {}
+};
+
+template <typename T> class singlyLinkedList {
+  private:
+  Node<T> *head;
+  int size;
+
+  public:
+  singlyLinkedList();
+  ~singlyLinkedList();
+
+  void append(T value);  // Append new value to linked list
+  void print();          // Print current linked list implementation
+  int getSize();         // return size
+};
+
+template <typename T> singlyLinkedList<T>::singlyLinkedList() : size(0), head(nullptr) {}
+
+template <typename T> singlyLinkedList<T>::~singlyLinkedList() {
+  Node<T> *curr = head;
+  while (curr != nullptr) {
+    Node<T> *next = curr->next;
+    delete curr;
+    curr = next;
+  }
+}
+
+template <typename T> void singlyLinkedList<T>::append(T value) {
+  if (size == 0) {
+    head = new Node<T>(value);
+  } else {
+    Node<T> *curr = head;
+    while (curr->next != nullptr) {
+      curr = curr->next;
+    }
+    curr->next = new Node<T>(value);
+  }
+  size++;
+}
+
+template <typename T> int singlyLinkedList<T>::getSize() { return size; }
+
+template <typename T> void singlyLinkedList<T>::print() {
+
+  Node<T> *curr = head;
+
+  while (curr != nullptr) {
+    std::cout << curr->data << "->";
+    curr = curr->next;
+  }
+  std::cout << "nullptr" << std::endl;
+}
--- testKnowledge/designAllLists/main.cpp
+++ testKnowledge/designAllLists/main.cpp
@@ -1,5 +1,5 @@
-
 #include "ArrayList.h"
+#include "SinglyLinkedList.h"
 // #include "DoublyLinkedList.h"
 // #include "SinglyLinkedList.h"
 #include <fstream>
@@ -8,15 +8,19 @@
 #include <string>
 
 const int MAX_LINES = 2643;
+const std::string FILE_NAME = "random-words.txt";
 
+// Generate a random number from 0 to 100000
+int randomNumber();
 // Generate a random word from a file
 std::string randomWord(std::ifstream &file, int maxLines);
 
 // Test case for ArrayList
 void arrayListTest(bool isInt, std::ifstream &inputFile);
 
+void singlyLinkedListTest(bool isInt, std::ifstream &inputTile);
+
 int main() {
-  const std::string FILE_NAME = "random-words.txt";
   std::ifstream inputFile(FILE_NAME);
 
   if (!inputFile.is_open()) {
@@ -28,9 +32,19 @@ int main() {
   arrayListTest(true, inputFile);
   std::cout << "Array List String Implementation" << std::endl << "----------" << std::endl;
   arrayListTest(false, inputFile);
+  std::cout << "Singly Linked List Int Implementation" << std::endl << "----------" << std::endl;
+  singlyLinkedListTest(true, inputFile);
+  std::cout << "Singly Linked List String Implementation" << std::endl << "----------" << std::endl;
+  singlyLinkedListTest(false, inputFile);
 
   inputFile.close();
   return 0;
+}
+
+int randomNumber() {
+  std::random_device rd;
+  std::uniform_int_distribution<int> dist(0, 100000);
+  return dist(rd);
 }
 
 std::string randomWord(std::ifstream &file, int maxLines) {
@@ -50,6 +64,27 @@ std::string randomWord(std::ifstream &file, int maxLines) {
   }
 
   return word;
+}
+
+void singlyLinkedListTest(bool isInt, std::ifstream &inputFile) {
+  if (isInt) {
+    singlyLinkedList<int> singlyLinkedList;
+
+    for (int i = 0; i < 20; i++) {
+      singlyLinkedList.append(randomNumber());
+    }
+    std::cout << "Size of array: " << singlyLinkedList.getSize() << std::endl;
+    singlyLinkedList.print();
+
+  } else {
+    singlyLinkedList<std::string> singlyLinkedList;
+
+    for (int i = 0; i < 20; i++) {
+      singlyLinkedList.append(randomWord(inputFile, MAX_LINES));
+    }
+    std::cout << "Size of array: " << singlyLinkedList.getSize() << std::endl;
+    singlyLinkedList.print();
+  }
 }
 
 void arrayListTest(bool isInt, std::ifstream &inputFile) {
