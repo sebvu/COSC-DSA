d2e57e5625a13c35a322596d1bc151b50979a42a
sebvu <santosjester738@gmail.com>
2024-11-01 01:40:13 -0500

    Fix logic errors with BST Add Function

--- homeworks/hw4/BST.h
+++ homeworks/hw4/BST.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:021555ec5b70755e2b6a94cac050af19478fb4b6555d8fc88d1a9fbf176def4a
-size 2515
+oid sha256:fd62ce37e192c6640da83babbd901353efc279c567f8359213933edc17ea3bfd
+size 2561
--- homeworks/hw4/WordMatcher.h
+++ homeworks/hw4/WordMatcher.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6210245b53b7116ef71d9599ecd346dd52866e8652e4d5434df55058d6ac183
-size 2196
+oid sha256:4ccf8c0614f9e2fab8f116f28e972401dc39c6d09f359d1bea12fa8040d86275
+size 2160
